169ef51bd07d37204db3c1bf887c0349f096081f
Giovanny Avila <gjavilae@gmail.com>
2022-09-29 12:27:42 -0400

    POO and inheritance in go

---
+++ day2/poo/inheritance/go.mod
@@ -0,0 +1,3 @@
+module example.com/poo/inherit
+
+go 1.18
---
+++ day2/poo/inheritance/inherit.go
@@ -0,0 +1,42 @@
+/*
+Learn from  "Build web application with colang"
+github https://github.com/astaxie/build-web-application-with-golang/blob/master/es/02.5.md
+*/
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	age int
+}
+
+
+type Student struct {
+	Human // this mark Employee be inherited from Human struct
+	university string
+}
+
+type Employee struct {
+	Human // this mark Employee be inherited from Human struct
+	company string
+}
+
+// Human struct method
+func (h *Human) sayHello() {
+	fmt.Printf("Hello! %v \n", h.name)
+}
+
+// Method overload for employee struct
+func (e *Employee) sayHello() {
+	// fmt.Printf("Hello %v!, you are employee at %v", e.Human.name, e.company)
+	fmt.Printf("Hello %v!, you are employee at %v", e.name, e.company)
+}
+
+func main() {
+	studen1 := Student{Human{"Giovanny", 27}, "LUZ/IUTM"}
+	employee1 := Employee{Human{"Jaimito", 32}, "GOOGLE"}
+
+	studen1.sayHello()
+	employee1.sayHello()
+}
