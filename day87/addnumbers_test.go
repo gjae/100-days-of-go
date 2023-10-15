64f7cb46643da8b8c1cb35947ec07ee2ea94718f
Gio <gjavilae@gmail.com>
2023-10-15 14:05:49 -0400

    Leetcode linkedlist exercise

---
+++ day87/addnumbers_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	log.Print("Running tests")
+	l1 := &ListNode{Val: 9, Next: &ListNode{Val: 9}}
+	l2 := &ListNode{Val: 9, Next: &ListNode{Val: 9}}
+	var arrAssert []int
+	result := []int{8, 9, 1}
+	response := addTwoNumbers(l1, l2)
+
+	for response != nil {
+		arrAssert = append(arrAssert, response.Val)
+		response = response.Next
+	}
+
+	assert.ElementsMatch(t, result, arrAssert, "Arrays aren't equals")
+
+}
